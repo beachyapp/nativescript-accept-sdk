f8851bd31c12ed0f7ea480000428021965eec895
Piotr Ilski <piotr.ilski@gmail.com>
2018-06-19 11:57:36 +0200

    Update NS demo

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -12,8 +12,22 @@ export class HelloWorldModel extends Observable {
     this.acceptSdk = new AcceptSdk();
     this.message = 'Getting token for test environment';
 
+    const cc = {
+      cardNumber: '4111111111111111',
+      expMonth: '11',
+      expYear: '22',
+      cvc: '256',
+    };
+
+    const config = {
+      clientName: '5KP3u95bQpv',
+      clientKey: '5FcB6WrfHGS76gHW3v7btBCE3HuuBuke9Pj96Ztfn5R32G5ep42vne7MCWZtAucY',
+      environment: 'test',
+    };
+
+
     this.acceptSdk
-      .getToken()
+      .getToken(config, cc)
       .then(token => {
         console.error("WE HAVE A TOKEN!!!");
         console.error(token);
